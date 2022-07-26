44fa959ce6e676ff2db0784afc82f6e044921363
Noah Chernet <noahchernet@yahoo.com>
2022-07-26 20:30:53 +0300

    CompaniesSection component made responsive

--- components/CompaniesSection.tsx
+++ components/CompaniesSection.tsx
@@ -1,48 +1,51 @@
-import { Box, Image, Spacer, Stack, Text } from "@chakra-ui/react";
+import { Box, Image, Spacer, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 type Props = {};
 
 const CompaniesSection = (props: Props) => {
   return (
-    <Box mx={"11.87rem"}>
-      <svg
-        width="65"
-        height="6"
-        viewBox="0 0 65 6"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <line
-          x1="3"
-          y1="3"
-          x2="62"
-          y2="3"
-          stroke="#ffca1d"
-          strokeWidth="6"
-          strokeLinecap="round"
-        />
-      </svg>
-
-      <Stack
-        direction={{ base: "column", md: "row" }}
-        pt={"0.9rem"}
-        spacing={"5.4rem"}
-      >
+    <Stack
+      px={{ base: "3rem", xl: "11.87rem" }}
+      direction={{ base: "column", xl: "row" }}
+    >
+      <VStack align="left">
+        <svg
+          width="65"
+          height="6"
+          viewBox="0 0 65 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line
+            x1="3"
+            y1="3"
+            x2="62"
+            y2="3"
+            stroke="#ffca1d"
+            strokeWidth="6"
+            strokeLinecap="round"
+          />
+        </svg>
         <Text
           color="#515151"
           fontSize={"1.875rem"}
           fontWeight="bold"
-          maxW={"33rem"}
+          maxW={{ xl: "33rem" }}
         >
           See why over 100,000 teams choose TeamFlow
         </Text>
-        <Spacer />
-        <Image
-          src="/assets/company_hulu.png"
-          alt="hulu_logo"
-          h={{ base: "2rem", md: "4rem" }}
-        />
+      </VStack>
+
+      <Stack
+        direction={{ base: "row" }}
+        // wrap={"wrap"}
+        pt={"0.9rem"}
+        spacing={"5.4rem"}
+        align={"center"}
+        w={"max-content"}
+      >
+        <Image src="/assets/company_hulu.png" alt="hulu_logo" h={"4rem"} />
         <Image src="/assets/company_bbc.png" alt="bbc_logo" h={"4rem"} />
         <Image
           src="/assets/company_universal.png"
@@ -51,7 +54,7 @@ const CompaniesSection = (props: Props) => {
         />
         <Image src="/assets/company_adobe.png" alt="adobe_logo" h={"4rem"} />
       </Stack>
-    </Box>
+    </Stack>
   );
 };
 
